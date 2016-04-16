e4681f6bc23e641c92d92ebf4c27c91659296db0
auda <brionmathieu@gmail.com>
2016-04-16 19:33:27 +0200

    second commit (tous les fichier oublier)

---
+++ www/js/app.js
@@ -0,0 +1,121 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+
+.controller('Enigme', function($scope) {
+
+    if (typeof $scope.NBenigme == 'undefined') {
+        $scope.NBenigme=1;
+        $scope.Resultat="Vous n'avez rien repondu";
+        $scope.Enonce="La reponse est : Initiation de lyha";
+        $scope.Reponse="Initiation de lyha";
+    }
+
+
+    $scope.plus = function() {
+        $scope.NBenigme++;
+        $scope.setEnoncer();
+    };
+    $scope.moin = function() {
+        if($scope.NBenigme>1){$scope.NBenigme--;}
+        $scope.setEnoncer();
+    };
+
+    $scope.setTexte = function() {
+       switch ($scope.NBenigme)
+       {   case 2:$scope.Enonce="enoncer 2";break;
+           case 3:$scope.Enonce="enoncer 3";break;
+           case 4:$scope.Enonce="enoncer 4";break;
+           case 5:$scope.Enonce="enoncer 5";break;
+          default:$scope.Enonce="enoncer 6";break;
+       }
+    };
+
+    $scope.setReponse = function() {
+        switch ($scope.NBenigme)
+        {   case 1:$scope.Reponse="Reponse ";break;
+            case 2:$scope.Reponse="Reponse ";break;
+            case 3:$scope.Reponse="Reponse ";break;
+            case 4:$scope.Reponse="Reponse ";break;
+            case 5:$scope.Reponse="Reponse ";break;
+           default:$scope.Reponse="Reponse ";break;
+        }
+    };
+
+
+    $scope.setEnoncer = function() {
+        $scope.Enonce="blabla";
+        $scope.Reponse="bidon "+$scope.NBenigme;
+        $scope.Resultat="Vous n'avez rien repondu";
+
+        $scope.setTexte();
+        $scope.setReponse();
+        
+    };
+
+    $scope.validation=function() {
+        $scope.autre="validation";
+        var current = document.getElementById('saisie').value;
+
+
+        if ($scope.Reponse == current) {
+
+            $scope.Resultat='Vous avez trouvé la bonne reponse';
+            $scope.plus();
+
+        }
+        else {$scope.Resultat='Vous vous etes trompés';}
+
+    };
+
+ })
+
+.config(function ($stateProvider,$urlRouterProvider) {
+  $stateProvider.state('home',{
+    url:'/home',
+    templateUrl:'templates/home.html'
+  })
+
+  $stateProvider.state('Jouer',{
+      url:'/Jouer',
+    templateUrl:'templates/Jouer.html'
+      })
+  
+  $stateProvider.state('Option',{ url:'/Option',
+    templateUrl:'templates/Option.html' })
+  
+  $stateProvider.state('Credit',{    url:'/Credit',
+    templateUrl:'templates/Credit.html'})
+  
+  $stateProvider.state('Enigme',{    url:'/Enigme',
+    templateUrl:'templates/Enigme.html',
+      controller:'Enigme'
+  })
+    
+  
+  
+  $urlRouterProvider.otherwise('/home')
+});
---
+++ www/js/function.js
@@ -0,0 +1,32 @@
+function Enigme_header(id) {document.getElementById('header').innerHTML ='Enigme .'+id;}
+function setHeaderbar(texte) {document.getElementById("header").innerHTML =texte;}
+function setEnoncerTexte(texte) {    document.getElementById("enoncer").innerHTML =texte;}
+function setTexte(ElementID,texte) {    document.getElementById(ElementID).innerHTML =texte;}
+function afficher( Id, variable) {    document.getElementById(Id).innerHTML =variable;}
+
+
+function setEnoncer(id) {
+    switch (id)
+    {case 1:setEnoncerTexte("La reponse est : demo");break;
+        case 2:setEnoncerTexte("enoncer 2");break;
+        case 3:setEnoncerTexte("enoncer 3");break;
+        case 4:setEnoncerTexte("enoncer 4");break;
+        case 5:setEnoncerTexte("enoncer 5");break;
+        case 6:setEnoncerTexte("enoncer 6");break;
+        default:setEnoncerTexte("depassement");break;
+    }
+}
+function reponse(id) {
+    switch (id)
+    {   case 1:return "bidon1";
+        case 2:return "bidon2";
+        case 3:return "bidon3";
+        case 4:return "bidon4";
+        case 5:return "bidon5";
+        case 6:return "bidon6";
+       default:return "bidon";
+    }
+}
+
+
+
